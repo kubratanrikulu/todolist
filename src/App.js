a72403135771e33ef254f06b3c715f90ed721bb4
Kübra Tanrıkulu <ktanrikuluu@gmail.com>
2022-04-10 13:04:25 +0300

    creat FormControl

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import logo from './logo.svg';
-import './App.css';
-import { Button } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { FormControl, Button } from 'react-bootstrap'
 
 function App() {
+  const [todo, setTodo] = useState('')
+  const addTodo = () => {
+    console.log(todo)
+    setTodo('')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>Hello World</div>
-        <Button>Tıkla</Button>
-      </header>
+    <div className="d-flex justify-content-center align-items-center flex-column mt-5">
+      <h2 className='mt-5'>Todo List</h2>
+      <div className='d-flex w-50'>
+        <FormControl
+          className='w-75 me-3 '
+          placeholder="Todo Input"
+          value={ todo }
+          onChange={(e) => setTodo(e.target.value)}/>
+        <Button onClick={addTodo} >Add todoList</Button>
+      </div>
+      <div>TodoBar</div>
     </div>
   );
 }
